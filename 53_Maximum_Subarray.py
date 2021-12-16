6422603f46288f2f7746c88f465b12659d52001c
Nigel <nigelandrewquinn@gmail.com>
2021-12-16 23:52:36 +0000

    medium

---
+++ 53_Maximum_Subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_so_far = nums[0]
+        max_ending_here = 0
+        for i in nums:
+            max_ending_here += i
+            if max_so_far < max_ending_here:
+                max_so_far = max_ending_here
+            if max_ending_here < 0:
+                max_ending_here = 0
+        return max_so_far
